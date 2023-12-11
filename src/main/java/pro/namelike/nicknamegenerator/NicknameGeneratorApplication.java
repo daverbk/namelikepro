9863fadc3cc46a286caaf976df1a6c9262a14533
David Rabko <davidryabko13@gmail.com>
2023-12-11 19:21:03 +0100

    Add initial commit

---
+++ src/main/java/pro/namelike/nicknamegenerator/NicknameGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pro.namelike.nicknamegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicknameGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicknameGeneratorApplication.class, args);
+    }
+
+}
