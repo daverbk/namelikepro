6c29257ca9be2159c2f2aeb7378d2c3c66ddcda5
David Rabko <david.rabko@icloud.com>
2023-12-11 19:21:03 +0100

    Add initial commit

---
+++ src/test/java/pro/namelike/nicknamegenerator/NicknameGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.namelike.nicknamegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicknameGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
