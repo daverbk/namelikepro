f8ddb1d42d901f58aee727e8753343b4df895f31
David Rabko <david.rabko@icloud.com>
2023-12-17 22:31:49 +0100

    Finish basic nickname generator
    Co-authored-by: Elovard <persall2013@gmail.com>

--- src/main/java/pro/namelike/nicknamegenerator/controller/HomeController.java
+++ src/main/java/pro/namelike/nicknamegenerator/controller/HomeController.java
@@ -7,51 +7,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pro.namelike.nicknamegenerator.entity.Nickname;
-import pro.namelike.nicknamegenerator.service.WordsService;
-
-import java.util.List;
-import java.util.Random;
+import pro.namelike.nicknamegenerator.service.NicknameGeneratorService;
 
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
-
-    private final WordsService wordsService;
+    private final NicknameGeneratorService nicknameGeneratorService;
 
     @GetMapping("/")
     public String getHome() {
         return "homepage";
     }
 
-    @PostMapping
+    @PostMapping("/generate")
     public ResponseEntity<String> generateNickname(@RequestBody Nickname nickname) {
-
-        String title;
-        if (nickname.isHasPostfix()) {
-            List<String> words = wordsService.getPrefixWordsList();
-            Random random = new Random();
-            title = words.get(random.nextInt(words.size()));
-        } else {
-            title = "";
-        }
-
-        String specialCharacter;
-        if (nickname.isHasSpecialCharacters()) {
-            specialCharacter = "***"; // add list with characters
-        } else {
-            specialCharacter = "";
-        }
-
-        String adjective;
-        if (nickname.isHasAdjective()) {
-            List<String> adjectives = wordsService.getAdjectivesWordsList();
-            Random random = new Random();
-            adjective = adjectives.get(random.nextInt(adjectives.size()));
-        } else {
-            adjective = "";
-        }
-
-        return ResponseEntity.ok(specialCharacter + adjective + nickname.getName() + nickname.getLastName() + title + specialCharacter);
-
+        return ResponseEntity.ok(nicknameGeneratorService.generate(nickname));
     }
 }
