9a5fc7c3fa28e582b9ad5a1e9fc202024c6464ce
Elovard <persall2013@gmail.com>
2023-12-26 16:14:44 +0300

    Update front end form & set up cors config

--- src/main/java/pro/namelike/nicknamegenerator/controller/NicknameGeneratorController.java
+++ src/main/java/pro/namelike/nicknamegenerator/controller/NicknameGeneratorController.java
@@ -3,17 +3,19 @@ package pro.namelike.nicknamegenerator.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pro.namelike.nicknamegenerator.dto.NicknameResponseDto;
 import pro.namelike.nicknamegenerator.entity.Nickname;
 import pro.namelike.nicknamegenerator.service.NicknameGeneratorService;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "*")
 public class NicknameGeneratorController {
     private final NicknameGeneratorService nicknameGeneratorService;
 
     @PostMapping("/generate")
-    public ResponseEntity<String> generateNickname(@RequestBody Nickname nickname) {
-        return ResponseEntity.ok(nicknameGeneratorService.generate(nickname));
+    public ResponseEntity<NicknameResponseDto> generateNickname(@RequestBody Nickname nickname) {
+        String generatedNickname = nicknameGeneratorService.generate(nickname);
+        return ResponseEntity.ok(new NicknameResponseDto(generatedNickname));
     }
 }
