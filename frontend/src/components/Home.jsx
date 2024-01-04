9e7c1e0560c2122ac69ca835d3bbb3b9092f7665
David Rabko <davidryabko13@gmail.com>
2024-01-04 21:26:15 +0300

    Add copy to clipboard button

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,14 +7,14 @@ class Home extends Component {
             <div>
                 <div className="site-info">
                     <h2 className="site-title">
-                        Name Like Pro
+                        Name It
                     </h2>
                     <div className="site-description">
                         <p>
                             Just enter two words and select if you want to add postfix / special characters / adjective :)
                             If all the checks are activated you'll get something like
                         </p>
-                        <code>☠ CringySHunKiller ☠</code>
+                        <code>߷ VibrantDaMaverick ߷</code>
                     </div>
                 </div>
                 <NicknameForm/>
