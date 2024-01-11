9450610463e1a1309a80e2688445eb596f488d26
David Rabko <david.rabko@icloud.com>
2024-01-11 16:33:19 +0100

    Fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,10 @@ const newHistory = createBrowserHistory();
 class App extends Component {
     render() {
         return (
-            <Router history={newHistory}>
+            <Router history={newHistory} basename={process.env.PUBLIC_URL}>
                 <Switch>
                     <Route path='/' exact={true} component={Home}/>
-                    <Route path='/about' component={AboutUs}/>
+                    <Route path='/about' exact={true} component={AboutUs}/>
                 </Switch>
             </Router>
         )
